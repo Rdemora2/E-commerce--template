bac72df2a336cf7a173088fd2e5aefd8076f8a68
Rdemora2 <robertomoraeszar@gmail.com>
2024-05-26 13:00:38 -0300

    BUILD/ add pinia dependency

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,14 @@ import type { NuxtConfig } from '@nuxt/types';
 const config: NuxtConfig = {
   devtools: { enabled: true },
   modules: [
-    'nuxt-primevue'
+    'nuxt-primevue',
+    '@pinia/nuxt'
   ],
   primevue: {
     usePrimeVue: true
+  },
+  alias: {
+    pinia: "/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs"
   },
 };
 export default config;
