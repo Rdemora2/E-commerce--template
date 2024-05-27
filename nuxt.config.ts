b3c763fc483934d4d0bdedf94458e20c5df92293
Rdemora2 <robertomoraeszar@gmail.com>
2024-05-26 22:16:52 -0300

    FEAT/ importa fonte do google fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -48,6 +48,12 @@ const config: NuxtConfig = {
     url: 'https://example.com',
     name: 'Awesome Site',
     description: 'Welcome to my awesome site!',
+  },
+  googleFonts: {
+    families: {
+      Poppins: [100, 200, 300, 400, 500, 600, 700, 800],
+
+    }
   }
 };
 export default config;
