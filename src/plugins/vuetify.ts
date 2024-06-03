a4972893e93d71266d99930d232edfb984ebf29f
Rdemora2 <robertomoraeszar@gmail.com>
2024-06-03 14:05:51 -0300

    FEAT/ define tema pádrao vuetify para claro

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -5,6 +5,9 @@ import { createVuetify } from 'vuetify'
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
+    theme: {
+      defaultTheme: 'light',
+    },
   })
   nuxtApp.vueApp.use(vuetify)
 })
