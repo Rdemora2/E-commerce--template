3c816c9b345123ef6691df0c1e585de9f52bf881
Rdemora2 <robertomoraeszar@gmail.com>
2024-06-04 13:18:20 -0300

    STYLE/ ajustes no estilo da navbar

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,24 +1,26 @@
 <template>
-  <nav>
-    <v-app-bar class="py-4 px-12 d-flex justify-center flex-wrap">
-      <v-toolbar-title class="ma-0">SHOP.CO</v-toolbar-title>
-      <v-btn text class="ml-2" to="/">Home</v-btn>
-      <v-btn text class="ml-2">Mulher</v-btn>
-      <v-btn text class="ml-2">Homem</v-btn>
-      <v-btn text class="ml-2">Sapatos</v-btn>
-      <v-btn text class="ml-2">Acessórios</v-btn>
-      <v-autocomplete
-        :items="items"
-        menu-icon=""
-        placeholder="Buscar Produtos..."
-        prepend-inner-icon="mdi-magnify"
-        variant="solo"
-        rounded
-        density="comfortable"
-      ></v-autocomplete>
-      <v-avatar>
-        <v-img src="https://cdn.vuetifyjs.com/images/lists/2.jpg"></v-img>
-      </v-avatar>
-    </v-app-bar>
-  </nav>
+  <div>
+    <nav>
+      <v-app-bar class="d-flex justify-center flex-wrap" style="position: relative; height: 10vh; padding: 1.6273vh 4.902vw;">
+        <v-toolbar-title class="ma-0">SHOP.CO</v-toolbar-title>
+        <v-btn text class="ml-2" to="/">Home</v-btn>
+        <v-btn text class="ml-2">Mulher</v-btn>
+        <v-btn text class="ml-2">Homem</v-btn>
+        <v-btn text class="ml-2">Sapatos</v-btn>
+        <v-btn text class="ml-2">Acessórios</v-btn>
+        <v-autocomplete
+          :items="items"
+          menu-icon=""
+          placeholder="Buscar Produtos..."
+          prepend-inner-icon="mdi-magnify"
+          variant="solo"
+          rounded
+          density="comfortable"
+        ></v-autocomplete>
+        <v-avatar>
+          <v-img src="https://cdn.vuetifyjs.com/images/lists/2.jpg"></v-img>
+        </v-avatar>
+      </v-app-bar>
+    </nav>
+  </div>
 </template>
