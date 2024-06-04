e0e08b9bb1484dba02aa73111608fba483e3cab0
Rdemora2 <robertomoraeszar@gmail.com>
2024-06-04 16:02:38 -0300

    STYLE/ insere estrelas de avaliação no card

--- src/components/productCard.vue
+++ src/components/productCard.vue
@@ -1,11 +1,9 @@
 <template>
-  <div class="d-flex justify-between mx-auto" style="width: 89vw; max-width: 1240px">
-    <v-card
-      max-height="410"
-      width="21vw"
-      max-width="300"
-      class="my-4 mx-2"
-    >
+  <div
+    class="d-flex justify-between mx-auto"
+    style="width: 89vw; max-width: 1240px"
+  >
+    <v-card max-height="410" width="21vw" max-width="300" class="my-4 mx-2">
       <v-img
         src="https://www.optimized-rlmedia.io/is/image/PoloGSI/s7-AI211943140001_alternate10?$rl_4x5_pdp$"
         max-height="290"
@@ -14,28 +12,29 @@
         aspect-ratio="1/1"
         cover
       ></v-img>
-  
-      <v-card-title class="font-weight-bold">
+
+      <v-card-title class="text-body-1 font-weight-bold mt-2">
         Polo Cropped Tricotado
       </v-card-title>
-  
-      <v-card-subtitle>
-        Avaliações
-      </v-card-subtitle>
-  
+
+      <div
+        class="d-flex flex-row align-center text-center mx-3"
+        style="height: 1vh"
+      >
+        <v-rating
+          v-model="rating"
+          density="comfortable"
+          size="x-small"
+          readonly
+          color="orange-lighten-1"
+        ></v-rating>
+      </div>
+
       <v-card-actions>
-        <v-btn
-          class="font-weight-bold"
-          text="R$150,00"
-        ></v-btn>
+        <v-btn class="font-weight-bold" text="R$150,00"></v-btn>
       </v-card-actions>
     </v-card>
-    <v-card
-      max-height="410"
-      width="21vw"
-      max-width="300"
-      class="my-4 mx-2"
-    >
+    <v-card max-height="410" width="21vw" max-width="300" class="my-4 mx-2">
       <v-img
         src="https://www.optimized-rlmedia.io/is/image/PoloGSI/s7-AI211943140001_alternate10?$rl_4x5_pdp$"
         max-height="290"
@@ -44,28 +43,29 @@
         aspect-ratio="1/1"
         cover
       ></v-img>
-  
-      <v-card-title class="font-weight-bold">
+
+      <v-card-title class="text-body-1 font-weight-bold mt-2">
         Polo Cropped Tricotado
       </v-card-title>
-  
-      <v-card-subtitle>
-        Avaliações
-      </v-card-subtitle>
-  
+
+      <div
+        class="d-flex flex-row align-center text-center mx-3"
+        style="height: 1vh"
+      >
+        <v-rating
+          v-model="rating"
+          density="comfortable"
+          size="x-small"
+          readonly
+          color="orange-lighten-1"
+        ></v-rating>
+      </div>
+
       <v-card-actions>
-        <v-btn
-          class="font-weight-bold"
-          text="R$150,00"
-        ></v-btn>
+        <v-btn class="font-weight-bold" text="R$150,00"></v-btn>
       </v-card-actions>
     </v-card>
-    <v-card
-      max-height="410"
-      width="21vw"
-      max-width="300"
-      class="my-4 mx-2"
-    >
+    <v-card max-height="410" width="21vw" max-width="300" class="my-4 mx-2">
       <v-img
         src="https://www.optimized-rlmedia.io/is/image/PoloGSI/s7-AI211943140001_alternate10?$rl_4x5_pdp$"
         max-height="290"
@@ -74,28 +74,29 @@
         aspect-ratio="1/1"
         cover
       ></v-img>
-  
-      <v-card-title class="font-weight-bold">
+
+      <v-card-title class="text-body-1 font-weight-bold mt-2">
         Polo Cropped Tricotado
       </v-card-title>
-  
-      <v-card-subtitle>
-        Avaliações
-      </v-card-subtitle>
-  
+
+      <div
+        class="d-flex flex-row align-center text-center mx-3"
+        style="height: 1vh"
+      >
+        <v-rating
+          v-model="rating"
+          density="comfortable"
+          size="x-small"
+          readonly
+          color="orange-lighten-1"
+        ></v-rating>
+      </div>
+
       <v-card-actions>
-        <v-btn
-          class="font-weight-bold"
-          text="R$150,00"
-        ></v-btn>
+        <v-btn class="font-weight-bold" text="R$150,00"></v-btn>
       </v-card-actions>
     </v-card>
-    <v-card
-      max-height="410"
-      width="21vw"
-      max-width="300"
-      class="my-4 mx-2"
-    >
+    <v-card max-height="410" width="21vw" max-width="300" class="my-4 mx-2">
       <v-img
         src="https://www.optimized-rlmedia.io/is/image/PoloGSI/s7-AI211943140001_alternate10?$rl_4x5_pdp$"
         max-height="290"
@@ -104,21 +105,35 @@
         aspect-ratio="1/1"
         cover
       ></v-img>
-  
-      <v-card-title class="font-weight-bold">
+
+      <v-card-title class="text-body-1 font-weight-bold mt-2">
         Polo Cropped Tricotado
       </v-card-title>
-  
-      <v-card-subtitle>
-        Avaliações
-      </v-card-subtitle>
-  
+
+      <div
+        class="d-flex flex-row align-center text-center mx-3"
+        style="height: 1vh"
+      >
+        <v-rating
+          v-model="rating"
+          density="comfortable"
+          size="x-small"
+          readonly
+          color="orange-lighten-1"
+        ></v-rating>
+      </div>
+
       <v-card-actions>
-        <v-btn
-          class="font-weight-bold"
-          text="R$150,00"
-        ></v-btn>
+        <v-btn class="font-weight-bold" text="R$150,00"></v-btn>
       </v-card-actions>
     </v-card>
   </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    rating: 4,
+  }),
+};
+</script>
