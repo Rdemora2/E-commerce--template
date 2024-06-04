267db860ce235401fa8cf3533ea933e093347e19
Rdemora2 <robertomoraeszar@gmail.com>
2024-06-04 13:18:46 -0300

    FEAT/ cria parallax para banner da home

---
+++ src/components/homeBanner.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-parallax
+      src="https://cdn-fsly.yottaa.net/620ab0e7d93140aaa4e17365/www.ralphlauren.global/v~4b.a0/on/demandware.static/-/Library-Sites-RalphLauren_EU_Library/en_BR/v1717484906206/img/202405/05232024-eu-w-polo-mb/0521_w_polo_plp_c01_img.jpg?yocs=4G_4V_4N_4W_"
+      height="67.5vh"
+      >
+      <div class="d-flex flex-column fill-height justify-center align-center text-white">
+        <h1 class="text-h4 font-weight-thin mb-4">
+          Vuetify
+        </h1>
+        <h4 class="subheading">
+          Build your application today!
+        </h4>
+      </div>
+    </v-parallax>
+  </div>
+  </template>
