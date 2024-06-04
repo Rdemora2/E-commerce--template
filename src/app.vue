0d69128e71c86ca28f10e7500a16a11e99f7d021
Rdemora2 <robertomoraeszar@gmail.com>
2024-06-04 16:34:32 -0300

    FEAT/ cria promobar superior

--- src/app.vue
+++ src/app.vue
@@ -1,6 +1,7 @@
 <template>
   <NuxtLayout>
         <v-app>
+            <promoBar />
             <navbar />
             <NuxtPage />
         </v-app>
---
+++ src/components/promoBar.vue
@@ -0,0 +1,7 @@
+<template>
+    <v-container fluid class="bg-black" style="height: 3vh;">
+      <v-row class="d-flex justify-center align-center">
+        <span class="text-subtitle-2">Cadastre-se e ganhe 20% de desconto no seu primeiro pedido.</span>
+      </v-row>
+    </v-container>
+  </template>
