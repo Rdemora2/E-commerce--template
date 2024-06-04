65303aed04bb4de7b683b9e8cdd2be3e9760564b
Rdemora2 <robertomoraeszar@gmail.com>
2024-06-04 17:02:26 -0300

    STYLE/ ajusta altura do banner homepage

--- src/components/homeBanner.vue
+++ src/components/homeBanner.vue
@@ -2,7 +2,7 @@
   <div>
     <v-parallax
       src="https://cdn-fsly.yottaa.net/620ab0e7d93140aaa4e17365/www.ralphlauren.global/v~4b.a0/on/demandware.static/-/Library-Sites-RalphLauren_EU_Library/en_BR/v1717484906206/img/202405/05232024-eu-w-polo-mb/0521_w_polo_plp_c01_img.jpg?yocs=4G_4V_4N_4W_"
-      height="67.5vh"
+      height="72vh"
       >
       <div class="d-flex flex-column fill-height justify-center align-center text-white">
         <h1 class="text-h4 font-weight-thin mb-4">
