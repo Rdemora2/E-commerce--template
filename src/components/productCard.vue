f7c00e3c58065c466d3794210eedb2eedf70f0b7
Rdemora2 <robertomoraeszar@gmail.com>
2024-06-04 15:31:10 -0300

    FEAT/ cria product card

---
+++ src/components/productCard.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="d-flex justify-between mx-auto" style="width: 89vw; max-width: 1240px">
+    <v-card
+      max-height="410"
+      width="21vw"
+      max-width="300"
+      class="my-4 mx-2"
+    >
+      <v-img
+        src="https://www.optimized-rlmedia.io/is/image/PoloGSI/s7-AI211943140001_alternate10?$rl_4x5_pdp$"
+        max-height="300"
+        width="100%"
+        max-width="300"
+        aspect-ratio="1/1"
+        cover
+      ></v-img>
+  
+      <v-card-title class="font-weight-bold">
+        Polo Cropped Tricotado
+      </v-card-title>
+  
+      <v-card-subtitle>
+        Avaliações
+      </v-card-subtitle>
+  
+      <v-card-actions>
+        <v-btn
+          class="font-weight-bold"
+          text="R$150,00"
+        ></v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-card
+      max-height="410"
+      width="21vw"
+      max-width="300"
+      class="my-4 mx-2"
+    >
+      <v-img
+        src="https://www.optimized-rlmedia.io/is/image/PoloGSI/s7-AI211943140001_alternate10?$rl_4x5_pdp$"
+        max-height="300"
+        width="100%"
+        max-width="300"
+        aspect-ratio="1/1"
+        cover
+      ></v-img>
+  
+      <v-card-title class="font-weight-bold">
+        Polo Cropped Tricotado
+      </v-card-title>
+  
+      <v-card-subtitle>
+        Avaliações
+      </v-card-subtitle>
+  
+      <v-card-actions>
+        <v-btn
+          class="font-weight-bold"
+          text="R$150,00"
+        ></v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-card
+      max-height="410"
+      width="21vw"
+      max-width="300"
+      class="my-4 mx-2"
+    >
+      <v-img
+        src="https://www.optimized-rlmedia.io/is/image/PoloGSI/s7-AI211943140001_alternate10?$rl_4x5_pdp$"
+        max-height="300"
+        width="100%"
+        max-width="300"
+        aspect-ratio="1/1"
+        cover
+      ></v-img>
+  
+      <v-card-title class="font-weight-bold">
+        Polo Cropped Tricotado
+      </v-card-title>
+  
+      <v-card-subtitle>
+        Avaliações
+      </v-card-subtitle>
+  
+      <v-card-actions>
+        <v-btn
+          class="font-weight-bold"
+          text="R$150,00"
+        ></v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-card
+      max-height="410"
+      width="21vw"
+      max-width="300"
+      class="my-4 mx-2"
+    >
+      <v-img
+        src="https://www.optimized-rlmedia.io/is/image/PoloGSI/s7-AI211943140001_alternate10?$rl_4x5_pdp$"
+        max-height="300"
+        width="100%"
+        max-width="300"
+        aspect-ratio="1/1"
+        cover
+      ></v-img>
+  
+      <v-card-title class="font-weight-bold">
+        Polo Cropped Tricotado
+      </v-card-title>
+  
+      <v-card-subtitle>
+        Avaliações
+      </v-card-subtitle>
+  
+      <v-card-actions>
+        <v-btn
+          class="font-weight-bold"
+          text="R$150,00"
+        ></v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
