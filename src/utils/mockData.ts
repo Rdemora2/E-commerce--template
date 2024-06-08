8ed8129cc017210a92ab2b5ee1f2b37aa87239ad
Rdemora2 <robertomoraeszar@gmail.com>
2024-06-08 01:20:03 -0300

    STYLE/ define medidas imagens faker

--- src/utils/mockData.ts
+++ src/utils/mockData.ts
@@ -6,7 +6,7 @@ export function generateMockProducts(count: number): Product[] {
   for (let i = 0; i < count; i++) {
     const product: Product = {
       name: faker.commerce.productName(),
-      photo: faker.image.url(),
+      photo: faker.image.url({ width: 300, height: 295 }),
       price: faker.commerce.price(),
       rating: faker.number.int({ min: 1, max: 5 }),
     };
