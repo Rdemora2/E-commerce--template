c9942135343e81c6965564d1727599e9abb38959
Rdemora2 <robertomoraeszar@gmail.com>
2024-06-08 01:00:35 -0300

    FEAT/ config mock data

---
+++ src/utils/mockData.ts
@@ -0,0 +1,24 @@
+import { faker } from "@faker-js/faker";
+
+export function generateMockProducts(count: number): Product[] {
+  const products: Product[] = [];
+
+  for (let i = 0; i < count; i++) {
+    const product: Product = {
+      name: faker.commerce.productName(),
+      photo: faker.image.imageUrl(),
+      price: faker.commerce.price(),
+      rating: faker.number.int({ min: 1, max: 5 }),
+    };
+    products.push(product);
+  }
+
+  return products;
+}
+
+export interface Product {
+  name: string;
+  photo: string;
+  price: string;
+  rating: number;
+}
